066f2f8b996737205b74a120d990faa9664c9e87
Gregory Kaczka <greg.kaczka@gmail.com>
2023-02-27 11:07:07 -0500

    Adding the service layer

--- SpringBootTutorial/demo/src/main/java/com/example/demo/controller/StudentController.java
+++ SpringBootTutorial/demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.ServiceLayer.StudentService;
 import com.example.demo.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,18 @@ import java.time.Month;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping(path = "/api/v1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
-        );
+        return studentService.getStudents();
     }
+
 }
