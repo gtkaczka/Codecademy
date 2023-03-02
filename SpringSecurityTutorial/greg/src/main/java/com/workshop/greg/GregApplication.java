0a2a9166a6d1f77bf1beb447b816fdf05dec7577
Gregory Kaczka <greg.kaczka@gmail.com>
2023-03-02 16:04:36 -0500

    adding the book entity and establishing relationships

---
+++ SpringSecurityTutorial/greg/src/main/java/com/workshop/greg/GregApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.greg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GregApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityTutorial/greg/src/test/java/com/workshop/greg/GregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.greg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
