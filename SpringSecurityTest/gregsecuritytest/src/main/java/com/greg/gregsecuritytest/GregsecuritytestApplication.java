c0c7fac5a8e49a3b03c6da125f46e9b27e5e14c1
Gregory Kaczka <greg.kaczka@gmail.com>
2023-03-02 17:55:14 -0500

    just pushing some code

---
+++ SpringSecurityTest/gregsecuritytest/src/main/java/com/greg/gregsecuritytest/GregsecuritytestApplication.java
@@ -0,0 +1,13 @@
+package com.greg.gregsecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GregsecuritytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GregsecuritytestApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityTest/gregsecuritytest/src/test/java/com/greg/gregsecuritytest/GregsecuritytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greg.gregsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GregsecuritytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
